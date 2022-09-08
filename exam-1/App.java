d277a4aff8050ae67e1bf274fd4c19c6ff41ee64
Franze Carlo Natividad <111488970+Barbatosss@users.noreply.github.com>
2022-09-08 14:38:13 +0800

    Create App.java

---
+++ exam-1/App.java
@@ -0,0 +1,16 @@
+public class App{
+    public static void main(String[] args) {
+        
+        Student Prans = new Student("Natividad ", "Franze Carlo ", "J.", "9/17/2004", true);
+
+        Prans.studentNumber = 2022103300;
+        Prans.sayMyStudentNumber();
+        Prans.studentEmailAddress = "natividadfj@students.national-u.edu.ph";
+        Prans.sayMyEmailAddress();
+        Prans.amIawesome = "OFCOURSE IM AWESOME!";
+        Prans.sayamIawesome();
+
+
+        
+    }
+}
